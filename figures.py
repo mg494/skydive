d3339780af6372c45ea137900c348fd7d6ca317e
Marc <marc.gehring@web.de>
2020-08-23 08:33:50 +0200

    write initial field as png

--- figures.py
+++ figures.py
@@ -6,8 +6,12 @@ casepath = "/home/marc/OpenFOAM/marc-7/run/"
 filename = casepath+casename+"/"+casename+".OpenFOAM"
 
 reader = OpenFOAMReader(FileName=filename)
-timesteps = reader.TimestepValues
+reader.SkipZeroTime = 0
+reader.Refresh()
 
+print(reader.SkipZeroTime)
+timesteps = reader.TimestepValues
+print(timesteps)
 Show()
 Render()
 
@@ -20,6 +24,22 @@ for line in lines:
 	match = re.search(r"\d{4}-\d{2}-\d{2}_\d{2}-\d{2}",line)
 	if match is not None: directories.append(match.group())
 if len(directories)<1: sys.exit("error: no entries in resultlog")
+
+# write initial field
+view = GetActiveView()
+layout = GetLayout()
+location = layout.GetViewLocation(view)
+layout.MaximizeCell(location)
+
+readerRep = GetRepresentation()
+ColorBy(readerRep, ("POINTS", "U"))
+UpdateScalarBars()
+
+view.ViewTime = 0
+
+Render()
+
+WriteImage("./storage/"+directories[-1]+"/velocity_t0.png")
 
 
 for time in timesteps:
